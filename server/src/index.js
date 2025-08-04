e58f5485906f7b753e0b8afa18cb28d2ea53921a
qwert619 <ebm619@gmail.com>
2025-08-04 00:13:52 -0500

    branch

--- server/src/index.js
+++ server/src/index.js
@@ -4,10 +4,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 const app = express();
-
+ 
+const databaseurl = 'mongodb://localhost:27017/datebite';
 
 app.use(cors());
 app.use(express.json());
 
+app.
 
 app.connect(3002, console.log('Server is running on port 3002'));

73de42feb1ed60220542d1306769cf04774ddeff
qwert619 <ebm619@gmail.com>
2025-08-05 14:29:32 -0500

    navbar

---
+++ client/src/components/navbar.js
@@ -0,0 +1,24 @@
+import { Link, useNavigate } from "react-router-dom";
+import { setCookies } from "react-cookie";
+
+export const Navbar = () => {
+    const navigate = useNavigate();
+    
+
+    return ( 
+        <div className="navbar">
+            <div className="navbar-container">
+            <Link to = "/location" className="location-link">
+                🌍
+            </Link>
+            <Link to ="/" className="logo-link">
+                <img src="/logo.png" alt="Logo" />
+            </Link>
+            <Link to = "/settings" className="settings-link">
+                ⚙️
+            </Link>
+            </div>
+        </div>
+    )
+
+}

9bb796060e08a895a539852b4a52c195643d7c61
qwert619 <ebm619@gmail.com>
2025-08-07 14:43:49 -0500

    location need controls

--- client/src/pages/location.js
+++ client/src/pages/location.js
@@ -1,10 +1,19 @@
-
+import React, { useState } from "react";
 
 export const Location = ({children}) => {
+    const [numberRestaurants, setNumberRestaurants] = useState(20);
+    const [distance, setDistance] = useState(4);
+
     // Get user location only once when component mounts
     return (
-        <div className="location">
-            {children}
+        <div className="location-settings">
+            <div className="location-controls">
+                <input type="number" placeholder="set number of resturants" defaultValue={20} onChange={(e) => setNumberRestaurants(e.target.value)} />
+                <input type="number" placeholder="set distance in miles" defaultValue={4} onChange={(e) => setDistance(e.target.value)} />
+            </div>
+            <div className="location">
+                {React.cloneElement(children, { numberRestaurants, distance })}
+        </div>
         </div>
     );
 };
